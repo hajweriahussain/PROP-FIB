3bcaaa11ef66efc1d65ec901bcd0d874bcbeb308
hajweria.hussain <hajweria.hussain@estudiantat.upc.edu>
2024-10-21 12:41:02 +0200

    Classe prestatgeria.java versió 1

---
+++ subgrup-prop41.1/src/CtrlDomini.java
@@ -0,0 +1,19 @@
+package Domini
+
+        public Class CtrlDomini
+        {
+            private Usuari UsuariActual;
+            private CjUsuaris cjUsuaris;
+            private CjtProductes CjtProductes;
+            private Prestatgeria Prestatgeria;
+            private tspAproximator tsp;
+            private BruteForceAlg bf;
+
+            private static CtrlDomini singletonObject;
+
+
+            public int crearPrestatgeria(){
+
+            }
+
+        }
---
+++ subgrup-prop41.1/src/Prestatgeria.java
@@ -0,0 +1,49 @@
+package Domini
+
+        public Class Prestatgeria
+        {
+            private Producte[][] layout;
+            private Integer id;
+            private int Files;
+            private int Columnes;
+
+
+            public Prestatgeria(int id, int nF, int nC){
+                this.id = id;
+                this.files = nF;
+                this.Columnes = nC;
+                this.layout = new Producte[files][Columnes];
+            }
+
+            public void canviarLayout(Producte[][] disp){
+                for (int i = 0; i < files;++i){
+                    for (int j = 0; j < columnes; ++j){
+                        layout[i][j] = disp[i][j];
+                    }
+                }
+            }
+
+            public Producte[][] getLayout(){
+                return layout;
+            }
+            public Integer getId(){
+                return id;
+            }
+            public int getNumF(){
+                return files;
+            }
+            public int getnumC(){
+                return Columnes;
+            }
+            public void setId(int id){
+                this.id = id;
+            }
+            public void setNumF(int nF){
+                this.files = nF;
+            }
+            public void setnumC(int nC){
+                this.Columnes = nC;
+            }
+
+
+        }
