867d458857ab10a30e497925f168b3cc92de0ca6
hajweria.hussain <hajweria.hussain@estudiantat.upc.edu>
2024-10-23 00:30:53 +0200

    Classe Usuari.java afegida + Actualització CtrlDomini.java

--- subgrup-prop41.1/src/FONTS/Domini/CtrlDomini.java
+++ subgrup-prop41.1/src/FONTS/Domini/CtrlDomini.java
@@ -1,19 +1,68 @@
 package Domini
 
-        public Class CtrlDomini
-        {
-            private Usuari UsuariActual;
-            private CjUsuaris cjUsuaris;
-            private CjtProductes CjtProductes;
-            private Prestatgeria Prestatgeria;
-            private tspAproximator tsp;
-            private BruteForceAlg bf;
+public Class CtrlDo
 
-            private static CtrlDomini singletonObject;
+import org.w3c.dom.Text;mini {
+    private Usuari UsuariActual;
+    private CjUsuaris cjUsuaris;
+    private CjProductes CjProductes;
+    //habra cjPrestatgerias?
+    private cjPrestatgerias cjPrestatgerias;
+    private Prestatgeria Prestatgeria;
+    private tspAproximator tsp;
+    private BruteForceAlg bf;
+    private float[][] MatSimilituds;
+    private Producte[][] MatProductes;
 
+    private static CtrlDomini singletonObject;
+    
+    public CtrlDomini() {
+        inicialitzarCtrlDomini();
+    }
 
-            public int crearPrestatgeria(){
+    public void inicialitzarCtrlDomini() {
+        cjUsuaris = new CjUsuaris(); //FALTA PARAMETRE
+        bf = new BruteForceAlg();
+    }
 
-            }
-
+    public void iniciarSessio(String username, String pwd){
+        if (cjUsuaris.getUsuari(username) == null){
+            nouUsuari(username, pwd);
         }
+        cjProductes = new cjProductes(username);
+        //cjPrestagerias si lo hay
+        cjPrestatgerias = new cjPrestatgerias(username);
+        Prestageria = novaPrestatgeria(); //LOS PARAMETROS DEBERIAN SER NFILES Y NCOLS. PERO DE DONDE LOS SACO
+        UsuariActual = cjUsuaris.getUsuari(username);
+    }
+
+    
+    public void nouUsuari(String name, String pwd){
+        cjUsuaris.crearUsuari(name, pwd);
+        iniciarSessio(name, pwd);
+    }
+
+    public void canviarUsuari(String nomUsuari, String pwd) {
+        //per a aquesta entrega només crida a iniciar sessió, però per les següents haurà de cridar a la base de dades i guardar el que calgui
+        iniciarSessio(nomUsuari, pwd); //canviem l'usuari actual
+    }
+    
+    public int novaPrestatgeria(int nf, int nc){
+        int id = cjPrestatgerias.crearPrestatgeria(nf, nc);
+
+    }
+
+    public void eliminarPrestatgeria(int id){
+        Prestatgeria p = cjPrestatgerias.getPrestatgeria(id);
+        cjPrestatgerias.esborrarPrestatgeria(p);
+    }
+
+    public String getUsuariActual() {
+        return UsuariActual.getUsername();
+    }
+
+    public void eliminarUsuari(){
+        cjUsuaris.esborrarUsuari(UsuariActual.getUsername());
+        tancarSessio(); //por implementar
+    }
+}
---
+++ subgrup-prop41.1/src/FONTS/Domini/Usuari.java
@@ -0,0 +1,29 @@
+package Domini
+
+public class Usuari {
+    private String username;
+    private String contrasenya;
+
+    public Usuari (String name, String contra){
+        username = name;
+        contrasenya = contra;
+    }
+
+    public Usuari getUsuari(){
+        return this;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+    public String getContrasenya(){
+        return contrasenya;
+    }
+
+    public void canviaUsername(String username){
+        this.username = username;
+    }
+    public void canviaContrasenya(String contra){
+        contrasenya = contra;
+    }
+}
