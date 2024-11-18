6c34f01fd13bd972d65f7a3a51223795693f029b
antonia.laura.apolzan <antonia.laura.apolzan@estudiantat.upc.edu>
2024-11-18 11:31:15 +0100

    detall driverBF i explicacio drivers

--- src/FONTS/Drivers/DriverBruteForce.java
+++ src/FONTS/Drivers/DriverBruteForce.java
@@ -139,7 +139,7 @@ public class DriverBruteForce {
         }
 
         else if(op == 2) {
-            System.out.println("Introdueix el nom del fitxer (que ha d'estar al directori DATA del projecte)");
+            System.out.println("Introdueix el nom del fitxer (que ha d'estar al directori ../EXE/Drivers/ del projecte)");
             String filename = sc.nextLine();
             String filePath = "../EXE/Drivers/" + filename;
 
