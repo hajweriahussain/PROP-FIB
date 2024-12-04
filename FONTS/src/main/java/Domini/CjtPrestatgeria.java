c2f19c626c5861ddd1a69a0b2e9e82c76a72dd1a
guillem.sturlese <guillem.sturlese@estudiantat.upc.edu>
2024-12-04 17:40:19 +0100

    correccio classe cnjprest

--- FONTS/src/main/java/Domini/CjtPrestatgeria.java
+++ FONTS/src/main/java/Domini/CjtPrestatgeria.java
@@ -6,14 +6,14 @@ import java.util.Set;
 
 public class CjtPrestatgeria {
     private String user;
-    private Map<int, Prestatgeria> map_prest;
+    private Map<Integer, Prestatgeria> map_prest;
 
     public CjtPrestatgeria(String u) {
         this.user = u;
         this.map_prest = new HashMap<>();
     }
 
-    public Map<int, Prestatgeria> getConjPrestatges(String userID) {
+    public Map<Integer, Prestatgeria> getConjPrestatges(String userID) {
         if (userID != null && user.equals(userID)) {
             return map_prest;
         }
@@ -24,39 +24,41 @@ public class CjtPrestatgeria {
         if (map_prest.containsKey(prestatgeID)) return map_prest.get(prestatgeID);
         //llença excepció
         System.out.println("Error: L'usuari no té cap prestatge amb aquesta id");
+        return null;
     }
 
-    /*
     public Producte[] getLayout(int prestatgeID) {
-        if map_prest.containsKey(prestatgeID) return (map_prest.get(prestatgeID).getLayout);
+        if (map_prest.containsKey(prestatgeID)) return (map_prest.get(prestatgeID).getLayout());
         //llença excepció
         System.out.println("Error: L'usuari no té cap prestatge amb aquesta id");
+        return null;
     }
 
     public int numProductes(int prestatgeID) {
-        if map_prest.containsKey(prestatgeID) return (map_prest.get(prestatgeID)).getNumProductes;
+        if (map_prest.containsKey(prestatgeID)) return (map_prest.get(prestatgeID)).getNumProductes();
         //llença excepció
         System.out.println("Error: L'usuari no té cap prestatge amb aquesta id");
-    }*/
+        return -1;
+    }
 
     public void afegeixPrestatgeria(Prestatgeria p) {
-        if (!map_prest.containsKey(p.getID())) {
+        if (!map_prest.containsKey(p.getId())) {
             map_prest.put(p.getId(), p);
         }
         System.out.println("Error: L'usuari ja té una prestatgeria amb aquest ID.");
     }
 
     public void editarPrestatgeria(Prestatgeria p) {
-        if (map_prest.containsKey(p.getID())) {
-            map_prest.remove(p.getID());
+        if (map_prest.containsKey(p.getId())) {
+            map_prest.remove(p.getId());
             map_prest.put(p.getId(), p);
         }
         System.out.println("Error: L'usuari no té una prestatgeria amb aquest ID.");
     }
 
     public void eliminarPrestatgeria(Prestatgeria p) {
-        if (map_prest.containsKey(p.getID())) {
-            map_prest.remove(p.getID());
+        if (map_prest.containsKey(p.getId())) {
+            map_prest.remove(p.getId());
         }
         System.out.println("Error: L'usuari no té una prestatgeria amb aquest ID.");
     }
