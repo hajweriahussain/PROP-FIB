2a3643b0d4598b6d6ba789bd3d3be9944b69af49
guillem.sturlese <guillem.sturlese@estudiantat.upc.edu>
2024-12-06 19:06:32 +0100

    classe cnjPrestatgeria arreglada

--- FONTS/src/main/java/Domini/CjtPrestatgeria.java
+++ FONTS/src/main/java/Domini/CjtPrestatgeria.java
@@ -2,7 +2,6 @@ package Domini;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class CjtPrestatgeria {
     private String user;
@@ -34,31 +33,75 @@ public class CjtPrestatgeria {
         return null;
     }
 
-    public int numProductes(int prestatgeID) {
-        if (map_prest.containsKey(prestatgeID)) return (map_prest.get(prestatgeID)).getNumProductes();
-        //llença excepció
-        System.out.println("Error: L'usuari no té cap prestatge amb aquesta id");
-        return -1;
-    }
-
-    public void afegeixPrestatgeria(Prestatgeria p) {
-        if (!map_prest.containsKey(p.getId())) {
-            map_prest.put(p.getId(), p);
+    public void crearPrestatgeria(int id, int filas, int columnas) { //crear
+        if (!map_prest.containsKey(id)) {
+            map_prest.put(id, new Prestatgeria(id, filas, columnas));
         }
         System.out.println("Error: L'usuari ja té una prestatgeria amb aquest ID.");
     }
 
-    public void editarPrestatgeria(Prestatgeria p) {
-        if (map_prest.containsKey(p.getId())) {
-            map_prest.remove(p.getId());
-            map_prest.put(p.getId(), p);
+    public void editarPrestatgeria(int id, int filas, int columnas) {
+        if (map_prest.containsKey(id)) {
+            map_prest.remove(id);
+            map_prest.put(id, new Prestatgeria(id, filas, columnas));
         }
         System.out.println("Error: L'usuari no té una prestatgeria amb aquest ID.");
     }
 
-    public void eliminarPrestatgeria(Prestatgeria p) {
-        if (map_prest.containsKey(p.getId())) {
-            map_prest.remove(p.getId());
+    public Producte[] getPrestatge(int prestatgeID, int indexFila) {
+        Prestatgeria prestatgeria = getPrestatgeria(prestatgeID);
+        if (prestatgeria != null) {
+            return prestatgeria.getPrestatge(indexFila);
+        } 
+        else {
+            System.out.println("Error: No es pot obtenir el prestatge.");
+            return null;
+        }
+    }
+
+    public void intercanviarDosProductes(int prestatgeID, int filaProd1, int colProd1, int filaProd2, int colProd2) {
+        Prestatgeria prestatgeria = getPrestatgeria(prestatgeID);
+        if (prestatgeria != null) {
+            prestatgeria.intercanviarDosProductes(filaProd1, colProd1, filaProd2, colProd2);
+        } 
+        else {
+            System.out.println("Error: No es pot intercanviar productes.");
+        }
+    }
+
+    public void afegirPrestatge(int prestatgeID) {
+        Prestatgeria prestatgeria = getPrestatgeria(prestatgeID);
+        if (prestatgeria != null) {
+            prestatgeria.afegirPrestatge();
+        } 
+        else {
+            System.out.println("Error: No es pot afegir un nou prestatge.");
+        }
+    }
+
+    public void esborrarPrestatge(int prestatgeID, int indexFila) {
+        Prestatgeria prestatgeria = getPrestatgeria(prestatgeID);
+        if (prestatgeria != null) {
+            prestatgeria.esborrarPrestatge(indexFila);
+        } 
+        else {
+            System.out.println("Error: No es pot esborrar el prestatge.");
+        }
+    }
+
+    public void esborrarPrestatgeria(int prestatgeID) {
+        Prestatgeria prestatgeria = getPrestatgeria(prestatgeID);
+        if (prestatgeria != null) {
+            prestatgeria.esborrarPrestatgeria();
+        } 
+        else {
+            System.out.println("Error: No es pot esborrar la prestatgeria.");
+        }
+    }
+
+    public void eliminarPrestatgeria(int id) {
+        if (map_prest.containsKey(id)) {
+            map_prest.remove(id);
         }
         System.out.println("Error: L'usuari no té una prestatgeria amb aquest ID.");
     }
